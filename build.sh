09417671c380685e0e944b537cca65f27c6ca1c2
Brian Rak <dn@devicenull.org>
2011-05-10 19:15:45 -0400

    Inital NS2Update import

---
+++ build.sh
@@ -0,0 +1,1 @@
+python src/setup.py py2exe
---
+++ src/ns2update.py
@@ -0,0 +1,82 @@
+#!/bin/python
+
+import urllib2, subprocess, sys, os, time, logging
+from xml.etree.ElementTree import parse
+from logging import debug, info, warning, error, critical
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='ns2update.log',
+                    filemode='a')
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+console.setFormatter(logging.Formatter('[NS2Update] %(levelname)-8s %(message)s'))
+logging.getLogger('').addHandler(console)
+
+versionURL = "http://ns2update.devicenull.org/ns2update/ns2version.txt"
+serverDir = os.getcwd()
+
+# if checkHash is set to 1, we will try to update until the hash of Server.exe changes
+def update(serverDir,checkHash):
+	info("Starting server update")
+	update = subprocess.Popen("../hldsupdatetool.exe -command update -game naturalselection2 -dir %s" % serverDir)
+	if update:
+		while update.returncode == None:
+			time.sleep(5)
+			update.poll()
+	info("Server update complete!")
+
+def startServer(args):
+	serverProc = subprocess.Popen("Server.exe %s" % args)
+	info("Server started, pid %i" % (serverProc.pid))
+	return serverProc
+	
+def stopServer(serverProc):
+	if serverProc != None:
+		info("Killing server")
+		serverProc.kill()
+
+
+serverArgs = " ".join(sys.argv[1:])
+debug("Command line args: %s" % (serverArgs))
+
+currentVersion = lastCheck = checkDelay = 0
+serverProc = None
+try:
+	while 1==1:
+		if time.time() - lastCheck > checkDelay:
+			debug("Checking for server update...")
+			data = urllib2.urlopen(versionURL)
+			
+			temp = data.read(100).split(",")
+			if temp[0] > 0 and temp[1] > 60:
+				desiredVersion = temp[0]
+				if checkDelay != int(temp[1]):
+					checkDelay = int(temp[1])
+					debug("Setting update check delay to %i" % checkDelay)
+			
+			if desiredVersion > currentVersion:	
+				info("Server is out of date: current: %s desired: %s" % (currentVersion,desiredVersion))
+				if serverProc != None:
+					stopServer(serverProc)
+				
+				if currentVersion == 0:
+					update(serverDir,0)
+					debug("Latest server version is %s" % (desiredVersion))
+				else:
+					update(serverDir,1)
+					
+				currentVersion = desiredVersion
+				serverProc = startServer(serverArgs)
+		
+		serverProc.poll()
+		if serverProc.returncode != None:
+			critical("Server has died, restarting!")
+			serverProc = startServer(serverArgs)
+			
+		time.sleep(10)
+except KeyboardInterrupt:
+	stopServer(serverProc)
+	sys.exit(0)
