39e13af2cf62e32617481223dd236465949dee09
Brian Rak <dn@devicenull.org>
2011-05-11 20:24:29 -0400

    Fix issue where server.exe would hang around after ns2update was killed.  Log all server console output to serverlogs/

--- src/ns2update.py
+++ src/ns2update.py
@@ -1,8 +1,11 @@
 #!/bin/python
 
-import urllib2, subprocess, sys, os, time, logging
+import urllib2, subprocess, sys, os, time, logging, signal, win32api
 from xml.etree.ElementTree import parse
 from logging import debug, info, warning, error, critical
+from threading import Thread
+from Queue import Queue, Empty
+from time import strftime
 
 
 logging.basicConfig(level=logging.DEBUG,
@@ -18,6 +21,13 @@ logging.getLogger('').addHandler(console)
 versionURL = "http://ns2update.devicenull.org/ns2update/ns2version.txt"
 serverDir = os.getcwd()
 
+def enqueue_output(out, queue, logFile):
+	for line in iter(out.readline,''):
+		queue.put(line)
+		logFile.write(line)
+		logFile.flush()
+	out.close()
+
 # if checkHash is set to 1, we will try to update until the hash of Server.exe changes
 def update(serverDir,checkHash):
 	info("Starting server update")
@@ -29,55 +39,95 @@ def update(serverDir,checkHash):
 	info("Server update complete!")
 
 def startServer(args):
-	serverProc = subprocess.Popen("Server.exe %s" % args)
+	# Actualyl start the server process
+	serverProc = subprocess.Popen("Server.exe %s" % args, stdin=None, stdout=subprocess.PIPE)
 	info("Server started, pid %i" % (serverProc.pid))
-	return serverProc
-	
-def stopServer(serverProc):
+
+	# Open up the log file
+	logName = strftime("%Y.%m.%d.%H%M.log")
+	debug("Logging to %s" % logName)
+	logFile = open("serverlogs/%s" % logName,"a")
+
+	# Setup everything we need to capture the server output
+	outQueue = Queue()
+	outThread = Thread(target=enqueue_output, args=(serverProc.stdout, outQueue, logFile))
+	outThread.daemon = True
+	outThread.start()
+
+	return (outThread, outQueue, serverProc, logFile)
+
+def stopServer(serverProc, outThread, logFile):
 	if serverProc != None:
 		info("Killing server")
 		serverProc.kill()
+		cleanupServer(serverProc, outThread, logFile)
 
+def cleanupServer(serverProc, outThread, logFile):
+	if outThread != None:
+		outThread.join()
+	if logFile != None:
+		logFile.close()
 
+def exitHandler(signalType):
+	info("Got signal %s" % (signalType))
+	if signalType == signal.CTRL_C_EVENT or signalType == signal.CTRL_BREAK_EVENT:
+		serverProc.kill()
+
+outQueue = Queue()
 serverArgs = " ".join(sys.argv[1:])
 debug("Command line args: %s" % (serverArgs))
 
 currentVersion = lastCheck = checkDelay = 0
-serverProc = None
+serverProc = outThread = outQueue = logFile = None
+
+win32api.SetConsoleCtrlHandler(exitHandler,1)
+try:
+	os.mkdir("serverlogs")
+except WindowsError:
+	pass
+
 try:
 	while 1==1:
 		if time.time() - lastCheck > checkDelay:
 			debug("Checking for server update...")
 			data = urllib2.urlopen(versionURL)
-			
+
 			temp = data.read(100).split(",")
 			if temp[0] > 0 and temp[1] > 60:
 				desiredVersion = temp[0]
 				if checkDelay != int(temp[1]):
 					checkDelay = int(temp[1])
 					debug("Setting update check delay to %i" % checkDelay)
-			
-			if desiredVersion > currentVersion:	
+
+			if desiredVersion > currentVersion:
 				info("Server is out of date: current: %s desired: %s" % (currentVersion,desiredVersion))
 				if serverProc != None:
-					stopServer(serverProc)
-				
+					stopServer(serverProc, outThread, logFile)
+
 				if currentVersion == 0:
 					update(serverDir,0)
 					debug("Latest server version is %s" % (desiredVersion))
 				else:
 					update(serverDir,1)
-					
+
 				currentVersion = desiredVersion
-				serverProc = startServer(serverArgs)
+				outThread, outQueue, serverProc, logFile = startServer(serverArgs)
 			lastCheck = time.time()
-		
+
 		serverProc.poll()
 		if serverProc.returncode != None:
 			critical("Server has died, restarting!")
-			serverProc = startServer(serverArgs)
-			
-		time.sleep(10)
+			cleanupServer(serverProc, outThread, logFile)
+			outThread, outQueue, serverProc, logFile = startServer(serverArgs)
+
+		# Use this to read console output without blocking
+		#try:
+		#	while 1==1:
+		#		line = outQueue.get_nowait()
+		#except Empty:
+		#	pass
+
+		time.sleep(5)
 except KeyboardInterrupt:
-	stopServer(serverProc)
+	stopServer(serverProc, outThread, logFile)
 	sys.exit(0)
