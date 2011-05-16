e4eaca98af2e1ed74d3f3852d427a69df1cec783
Brian Rak <dn@devicenull.org>
2011-05-15 20:24:46 -0400

    Move core functionality over to NS2Update class, add basic console app functionality

--- src/ns2update.py
+++ src/ns2update.py
@@ -1,25 +1,8 @@
-#!/bin/python
-
-import urllib2, subprocess, sys, os, time, logging, signal
-from xml.etree.ElementTree import parse
-from logging import debug, info, warning, error, critical
+import urllib2, subprocess, sys, os, time
 from threading import Thread
 from Queue import Queue, Empty
 from time import strftime
 
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                    datefmt='%m-%d %H:%M',
-                    filename='ns2update.log',
-                    filemode='a')
-console = logging.StreamHandler()
-console.setLevel(logging.DEBUG)
-console.setFormatter(logging.Formatter('[NS2Update] %(levelname)-8s %(message)s'))
-logging.getLogger('').addHandler(console)
-
-versionURL = "http://ns2update.devicenull.org/ns2update/ns2version.txt"
-serverDir = os.getcwd()
 
 def enqueue_output(out, queue, logFile):
 	for line in iter(out.readline,''):
@@ -28,115 +11,122 @@ def enqueue_output(out, queue, logFile):
 		logFile.flush()
 	out.close()
 
-# if checkHash is set to 1, we will try to update until the hash of Server.exe changes
-def update(serverDir,checkHash):
-	info("Starting server update")
-	paths = [ "../hldsupdatetool.exe", "hldsupdatetool.exe" ]
-	if os.path.exists("%s/ns2update.cfg" % (serverDir)):
-		debug("Found ns2update.cfg file, loading hldsupdatetool path")
-		cfg = open("%s/ns2update.cfg" % (serverDir),"r")
-		paths.append(cfg.readline())
-		cfg.close()
+class NS2Update:
+	# Handle to the process where the server is running
+	serverProc = None
+	# outputThread will be a seperate thread that is spawned to grab all the output from the server
+	# It will push it all into outputQueue, as well as to serverLogFile
+	# outputQueue is currently unused, but will ultimately allow actions to be taken based on log events
+	outputQueue = None
+	outputThread = None
+	serverLogFile = None
+	# Time of the last update check
+	lastCheck = 0
+	# How often we should check for updates.  This is set by the update server
+	checkDelay = 300
+	# What URL to grab the latest version from.  Ideally we would grab this from the CDR on the fly
+	# but that's not terribly resource-friendly to do
+	versionURL = "http://ns2update.devicenull.org/ns2update/ns2version.txt"
+	# What version of the software is the server currently running
+	currentVersion = 0
+	# What arguments should we pass to the server on startup
+	serverArgs = ''
 
-	updatePath = ""
-	for cur in paths:
-		if os.path.exists(cur):
-			updatePath = cur
+	def __init__(self, logger, UpdateToolPath, serverDirectory, serverArgs):
+		self.logger = logger
+		self.findUpdateTool(UpdateToolPath)
+		self.serverDir = serverDirectory
+		self.serverArgs = serverArgs
 
-	if updatePath == "":
-		critical("Unable to find hldsupdatetool.exe, please copy to server directory or see README")
-		sys.exit(-1)
+	def findUpdateTool(self,extraPath):
+		paths = [ "../hldsupdatetool.exe", "hldsupdatetool.exe" ]
 
-	debug("Found hldsupdatetool at %s" % updatePath)
+		if extraPath != None and extraPath != '':
+			paths.append(extraPath)
 
-	update = subprocess.Popen("%s -command update -game naturalselection2 -dir %s" % (updatePath,serverDir))
-	if update:
-		while update.returncode == None:
-			time.sleep(5)
-			update.poll()
-	info("Server update complete!")
+		self.updatePath = ""
+		for cur in paths:
+			if os.path.exists(cur):
+				self.updatePath = cur
 
-def startServer(args):
-	# Actualyl start the server process
-	serverProc = subprocess.Popen("Server.exe %s" % args, stdin=None, stdout=subprocess.PIPE)
-	info("Server started, pid %i" % (serverProc.pid))
+		if self.updatePath == "":
+			self.logger.critical("Unable to find hldsupdatetool.exe, please copy to server directory or see README")
+			raise NameError("Unable to find hldsupdatetool.exe")
 
-	# Open up the log file
-	logName = strftime("%Y.%m.%d.%H%M.log")
-	debug("Logging to %s" % logName)
-	logFile = open("serverlogs/%s" % logName,"a")
+	def doUpdate(self):
+		self.logger.info("Starting server update")
 
-	# Setup everything we need to capture the server output
-	outQueue = Queue()
-	outThread = Thread(target=enqueue_output, args=(serverProc.stdout, outQueue, logFile))
-	outThread.daemon = True
-	outThread.start()
+		update = subprocess.Popen("%s -command update -game naturalselection2 -dir %s" % (self.updatePath,self.serverDir))
+		if update:
+			while update.returncode == None:
+				time.sleep(5)
+				update.poll()
 
-	return (outThread, outQueue, serverProc, logFile)
+		self.logger.info("Server update complete!")
 
-def stopServer(serverProc, outThread, logFile):
-	if serverProc != None:
-		info("Killing server")
-		serverProc.kill()
-		cleanupServer(serverProc, outThread, logFile)
+	def startServer(self):
+		# Actually start the server process
+		self.serverProc = subprocess.Popen("Server.exe %s" % self.serverArgs, stdin=None, stdout=subprocess.PIPE)
+		self.logger.info("Server started, pid %i" % (self.serverProc.pid))
 
-def cleanupServer(serverProc, outThread, logFile):
-	if outThread != None:
-		outThread.join()
-	if logFile != None:
-		logFile.close()
+		# Open up the log file
+		logName = strftime("%Y.%m.%d.%H%M.log")
+		self.logger.debug("Logging to %s" % logName)
+		self.serverLogFile = open("serverlogs/%s" % logName,"a")
 
-def exitHandler(signalType, stack):
-	global serverProc
-	info("Got signal %s" % (signalType))
-	serverProc.kill()
+		# Setup everything we need to capture the server output
+		self.outputQueue = Queue()
+		self.outputThread = Thread(target=enqueue_output, args=(self.serverProc.stdout, self.outputQueue, self.serverLogFile))
+		self.outputThread.daemon = True
+		self.outputThread.start()
 
-outQueue = Queue()
-serverArgs = " ".join(sys.argv[1:])
-debug("Command line args: %s" % (serverArgs))
+	def stopServer(self):
+		if self.serverProc != None:
+			self.logger.info("Killing server")
+			self.serverProc.kill()
+			self.cleanupServer()
 
-currentVersion = lastCheck = checkDelay = 0
-serverProc = outThread = outQueue = logFile = None
+	def cleanupServer(self):
+		if self.outputThread != None:
+			self.outputThread.join()
+		if self.serverLogFile != None:
+			self.serverLogFile.close()
+		self.outputThread = None
+		self.serverLogFile = None
+		self.serverProc = None
 
-signal.signal(signal.SIGINT, exitHandler)
-try:
-	os.mkdir("serverlogs")
-except WindowsError:
-	pass
-
-try:
-	while 1==1:
-		if time.time() - lastCheck > checkDelay:
-			debug("Checking for server update...")
-			data = urllib2.urlopen(versionURL)
+	def think(self):
+		if time.time() - self.lastCheck > self.checkDelay:
+			self.logger.debug("Checking for server update...")
+			data = urllib2.urlopen(self.versionURL)
 
 			temp = data.read(100).split(",")
 			if temp[0] > 0 and temp[1] > 60:
 				desiredVersion = temp[0]
-				if checkDelay != int(temp[1]):
-					checkDelay = int(temp[1])
-					debug("Setting update check delay to %i" % checkDelay)
+				if self.checkDelay != int(temp[1]):
+					self.checkDelay = int(temp[1])
+					self.logger.debug("Setting update check delay to %i" % self.checkDelay)
 
-			if desiredVersion > currentVersion:
-				info("Server is out of date: current: %s desired: %s" % (currentVersion,desiredVersion))
-				if serverProc != None:
-					stopServer(serverProc, outThread, logFile)
+			if desiredVersion > self.currentVersion:
+				self.logger.info("Server is out of date: current: %s desired: %s" % (self.currentVersion,desiredVersion))
+				if self.serverProc != None:
+					self.stopServer()
 
-				if currentVersion == 0:
-					update(serverDir,0)
-					debug("Latest server version is %s" % (desiredVersion))
+				if self.currentVersion == 0:
+					self.logger.info("Performing inital server update")
+					self.doUpdate()
 				else:
-					update(serverDir,1)
+					self.doUpdate()
 
-				currentVersion = desiredVersion
-				outThread, outQueue, serverProc, logFile = startServer(serverArgs)
-			lastCheck = time.time()
+				self.currentVersion = desiredVersion
+				self.startServer()
+			self.lastCheck = time.time()
 
-		serverProc.poll()
-		if serverProc.returncode != None:
-			critical("Server has died, restarting!")
-			cleanupServer(serverProc, outThread, logFile)
-			outThread, outQueue, serverProc, logFile = startServer(serverArgs)
+		self.serverProc.poll()
+		if self.serverProc.returncode != None:
+			self.logger.critical("Server has died, restarting!")
+			self.cleanupServer()
+			self.startServer()
 
 		# Use this to read console output without blocking
 		#try:
@@ -144,11 +134,3 @@ try:
 		#		line = outQueue.get_nowait()
 		#except Empty:
 		#	pass
-
-		time.sleep(5)
-except KeyboardInterrupt:
-	stopServer(serverProc, outThread, logFile)
-	sys.exit(0)
-except IOError:
-	stopServer(serverProc, outThread, logFile)
-	sys.exit(0)
