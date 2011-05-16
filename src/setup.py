323cf9859975537a36cf8fa9df0374a46e6a70a8
Brian Rak <dn@devicenull.org>
2011-05-15 20:32:32 -0400

    Generate proper exe type

--- src/setup.py
+++ src/setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 import py2exe
 
-setup(windows=[{'script':'src/console.py','dest_base':'ns2update'}])
+setup(console=[{'script':'src/console.py','dest_base':'ns2update'}])
