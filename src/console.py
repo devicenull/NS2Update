1d5129878e46c07724980accbcc7369bb5c92831
Brian Rak <dn@devicenull.org>
2011-05-15 20:31:12 -0400

    Fix exe file name, readd config file functionality

--- src/console.py
+++ src/console.py
@@ -1,12 +1,7 @@
 #!/bin/python
-
-import urllib2, subprocess, sys, os, time, logging, signal
-from xml.etree.ElementTree import parse
+import logging, sys, time, signal, os
 from logging import debug, info, warning, error, critical
-from threading import Thread
-from Queue import Queue, Empty
-from time import strftime
-
+from ns2update import NS2Update
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
@@ -17,138 +12,42 @@ console = logging.StreamHandler()
 console.setLevel(logging.DEBUG)
 console.setFormatter(logging.Formatter('[NS2Update] %(levelname)-8s %(message)s'))
 logging.getLogger('').addHandler(console)
+logger = logging.getLogger('')
 
-versionURL = "http://ns2update.devicenull.org/ns2update/ns2version.txt"
-serverDir = os.getcwd()
 
-def enqueue_output(out, queue, logFile):
-	for line in iter(out.readline,''):
-		queue.put(line)
-		logFile.write(line)
-		logFile.flush()
-	out.close()
-
-# if checkHash is set to 1, we will try to update until the hash of Server.exe changes
-def update(serverDir,checkHash):
-	info("Starting server update")
-	paths = [ "../hldsupdatetool.exe", "hldsupdatetool.exe" ]
-	if os.path.exists("%s/ns2update.cfg" % (serverDir)):
-		debug("Found ns2update.cfg file, loading hldsupdatetool path")
-		cfg = open("%s/ns2update.cfg" % (serverDir),"r")
-		paths.append(cfg.readline())
-		cfg.close()
-
-	updatePath = ""
-	for cur in paths:
-		if os.path.exists(cur):
-			updatePath = cur
-
-	if updatePath == "":
-		critical("Unable to find hldsupdatetool.exe, please copy to server directory or see README")
-		sys.exit(-1)
-
-	debug("Found hldsupdatetool at %s" % updatePath)
-
-	update = subprocess.Popen("%s -command update -game naturalselection2 -dir %s" % (updatePath,serverDir))
-	if update:
-		while update.returncode == None:
-			time.sleep(5)
-			update.poll()
-	info("Server update complete!")
-
-def startServer(args):
-	# Actualyl start the server process
-	serverProc = subprocess.Popen("Server.exe %s" % args, stdin=None, stdout=subprocess.PIPE)
-	info("Server started, pid %i" % (serverProc.pid))
-
-	# Open up the log file
-	logName = strftime("%Y.%m.%d.%H%M.log")
-	debug("Logging to %s" % logName)
-	logFile = open("serverlogs/%s" % logName,"a")
-
-	# Setup everything we need to capture the server output
-	outQueue = Queue()
-	outThread = Thread(target=enqueue_output, args=(serverProc.stdout, outQueue, logFile))
-	outThread.daemon = True
-	outThread.start()
-
-	return (outThread, outQueue, serverProc, logFile)
-
-def stopServer(serverProc, outThread, logFile):
-	if serverProc != None:
-		info("Killing server")
-		serverProc.kill()
-		cleanupServer(serverProc, outThread, logFile)
-
-def cleanupServer(serverProc, outThread, logFile):
-	if outThread != None:
-		outThread.join()
-	if logFile != None:
-		logFile.close()
-
-def exitHandler(signalType, stack):
-	global serverProc
-	info("Got signal %s" % (signalType))
-	serverProc.kill()
-
-outQueue = Queue()
 serverArgs = " ".join(sys.argv[1:])
+serverDir = os.getcwd()
 debug("Command line args: %s" % (serverArgs))
 
-currentVersion = lastCheck = checkDelay = 0
-serverProc = outThread = outQueue = logFile = None
+def exitHandler(signalType, stack):
+	global updater
+	info("Got signal %s" % (signalType))
+	updater.stopServer()
 
-signal.signal(signal.SIGINT, exitHandler)
 try:
 	os.mkdir("serverlogs")
 except WindowsError:
 	pass
 
+updatePath = ''
+if os.path.exists("%s/ns2update.cfg" % (serverDir)):
+	debug("Found ns2update.cfg file, loading hldsupdatetool path")
+	cfg = open("%s/ns2update.cfg" % (serverDir),"r")
+	updatePath = cfg.readline()
+	cfg.close()
+
+updater = NS2Update(logger=logging.getLogger(''),UpdateToolPath=None,serverDirectory = serverDir,serverArgs=serverArgs)
+
+# Don't define the exit handler until after the updater object is available
+signal.signal(signal.SIGINT, exitHandler)
+
 try:
 	while 1==1:
-		if time.time() - lastCheck > checkDelay:
-			debug("Checking for server update...")
-			data = urllib2.urlopen(versionURL)
-
-			temp = data.read(100).split(",")
-			if temp[0] > 0 and temp[1] > 60:
-				desiredVersion = temp[0]
-				if checkDelay != int(temp[1]):
-					checkDelay = int(temp[1])
-					debug("Setting update check delay to %i" % checkDelay)
-
-			if desiredVersion > currentVersion:
-				info("Server is out of date: current: %s desired: %s" % (currentVersion,desiredVersion))
-				if serverProc != None:
-					stopServer(serverProc, outThread, logFile)
-
-				if currentVersion == 0:
-					update(serverDir,0)
-					debug("Latest server version is %s" % (desiredVersion))
-				else:
-					update(serverDir,1)
-
-				currentVersion = desiredVersion
-				outThread, outQueue, serverProc, logFile = startServer(serverArgs)
-			lastCheck = time.time()
-
-		serverProc.poll()
-		if serverProc.returncode != None:
-			critical("Server has died, restarting!")
-			cleanupServer(serverProc, outThread, logFile)
-			outThread, outQueue, serverProc, logFile = startServer(serverArgs)
-
-		# Use this to read console output without blocking
-		#try:
-		#	while 1==1:
-		#		line = outQueue.get_nowait()
-		#except Empty:
-		#	pass
-
+		updater.think()
 		time.sleep(5)
 except KeyboardInterrupt:
-	stopServer(serverProc, outThread, logFile)
+	updater.stopServer()
 	sys.exit(0)
 except IOError:
-	stopServer(serverProc, outThread, logFile)
+	updater.stopServer()
 	sys.exit(0)
